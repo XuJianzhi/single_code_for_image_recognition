1adee144e0adfc12a5294d09f1ff32b03711f55d
XuJianzhi <30584453+XuJianzhi@users.noreply.github.com>
2018-01-10 16:27:08 +0800

    Update image_recognition.py

--- image_recognition.py
+++ image_recognition.py
@@ -4,7 +4,7 @@ from keras.preprocessing import image
 import numpy as np
 
 model = VGG16(weights='imagenet')  # 224×224
-img_path = 'elephant.jpg'
+img_path = 'pig.jpg'
 img = image.load_img(img_path, target_size=(224, 224))  # 224×224
 x = image.img_to_array(img)     # (3, 224, 224)
 x = np.expand_dims(x, axis=0)  # (1, 3, 224, 224)
